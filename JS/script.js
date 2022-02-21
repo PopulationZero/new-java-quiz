d980e5344e502b9d8ea376bf00573a9622a2f1b4
Alex Haas <alexzhaas@gmail.com>
2022-02-21 02:05:48 -0700

    added javascript. work in prog.

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,105 @@
-let questions = [{
-        question: "question 1",
-        choice1: '1',
-        choice2: '2',
-        choice3: '3',
-        choice4: '4',
-        answer: 1,
-    },
-    {
-        question: "question 2",
-        choice1: '5',
-        choice2: '6',
-        choice3: '7',
-        choice4: '8',
-        answer: 2,
-    },
-    {
-        question: "question 3",
-        choice1: '9',
-        choice2: '1',
-        choice3: '2',
-        choice4: '3',
-        answer: 3,
-    },
-    {
-        question: "question 4",
-        choice1: '4',
-        choice2: '5',
-        choice3: '6',
-        choice4: '7',
-        answer: 4,
-    },
-]
+const startButton = document.getElementById("start-btn")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+const showQuestion1 = document.getElementById("question", "answer-buttons1")
+const showQuestion2 = document.getElementById("question", "answer-buttons2")
+const showQuestion3 = document.getElementById("question", "answer-buttons3")
+const showQuestion4 = document.getElementById("question", "answer-buttons4")
 
-var questionHeaderEl = document.querySelector(".question");
-var answerBoxEl = document.querySelector(".answers");
-var currentQuestion = 0;
-var userScore = 60;
+
+function startQuiz() {
+    console.log("stardsa")
+    getNewQuestion();
+    countdown();
+
+}
+
+startQuiz();
+
+function getNewQuestion() {
+    array.forEach(question => {
+        showQuestion();
+    });
+
+
+}
+
+function showQuestion() {
+    question.classList.remove("hide");
+}
+
+function answerSelect() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var getNewQuestion = function() {
+//     if (document.querySelector(".show")) {
+//         document.querySelector(".show").remove();
+//     }
+//     // questionHeaderEl.removeChild(document.createElement("h1"));
+//     if (!questions[currentQuestion]) {
+//         return;
+//     }
+
+//     var questionEl = document.createElement("h1");
+//     questionEl.textContent = questions[currentQuestion].question;
+//     questionHeaderEl.appendChild(questionEl);
+
+
+//     var answersEl = document.createElement("div");
+//     answersEl.className = "show"
+
+//     answerBoxEl.appendChild(answersEl)
+
+//     for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+//         var buttonEl = document.createElement("button");
+//         buttonEl.textContent = questions[currentQuestion].choices[i];
+//         answersEl.appendChild(buttonEl);
+//     }
+// }
+
+// var answerSelect() {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var questionHeaderEl = document.querySelector(".question");
+// var answerBoxEl = document.querySelector(".answers");
+// var currentQuestion = 0;
+// var userScore = 60;
 
 
 var timerEl = document.getElementById("countdown")
 
 function countdown() {
-    var timeLeft = 10;
+    var timeLeft = 60;
 
     var timeInterval = setInterval(function() {
             if (timeLeft > 1) {
@@ -59,37 +117,3 @@ function countdown() {
         },
         1000);
 }
-
-function startQuiz() {
-    countdown();
-    // document.querySelector("#start").remove();
-    getNewQuestion();
-}
-
-var getNewQuestion = function() {
-    if (document.querySelector(".show")) {
-        document.querySelector(".show").remove();
-    }
-    // questionHeaderEl.removeChild(document.createElement("h1"));
-    if (!questions[currentQuestion]) {
-        return;
-    }
-
-    var questionEl = document.createElement("h1");
-    questionEl.textContent = questions[currentQuestion].question;
-    questionHeaderEl.appendChild(questionEl);
-
-
-    var answersEl = document.createElement("div");
-    answersEl.className = "show"
-
-    answerBoxEl.appendChild(answersEl)
-
-    for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-        var buttonEl = document.createElement("button");
-        buttonEl.textContent = questions[currentQuestion].choices[i];
-        answersEl.appendChild(buttonEl);
-    }
-}
-
-startQuiz();
