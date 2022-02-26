ec5cd39cc623472f55f158da3b24d23a56fc2bc1
Alex Haas <alexzhaas@gmail.com>
2022-02-26 11:00:45 -0700

    added questions

--- JS/script.js
+++ JS/script.js
@@ -7,15 +7,17 @@
 // var showQuestion4 = document.getElementById("question", "answer-buttons4")
 
 
-let question = {
-    title: "gato",
-    alt: ["dog", "cat", "bird", "elephant"],
-    correctAnswer: 1
+
+// -- 
+let question1 = {
+    question: "question 1",
+    alt: ["ans1", "ans1", "ans1", "ans1"],
+    correctAnswer1: 1
 }
 
 function showQuestion(q) {
-    var titleDiv = document.getElementById("title");
-    titleDiv.textContent = q.title;
+    var qDiv = document.getElementById("questions");
+    qDiv.textContent = q.question;
 
     var alts = document.querySelectorAll(".alt");
 
@@ -24,120 +26,153 @@ function showQuestion(q) {
 
         element.addEventListener("click", function() {
 
-            if (q.correctAnswer == index) {
+            if (q.correctAnswer1 == index) {
                 console.log("correct");
+                showQuestion2(question2);
+
             } else {
-                console.log("wrong answer")
-                    // minus points and go to next question (2)
+                console.log("wrong answedr")
+                timeLeft.subtract(10, "s")
+                showQuestion2(question2);
+                // timeLeft = (timeLeft.now() - 10000);
+                // minus points and go to next question (2) 
             }
         })
     });
+    return;
 };
 
-showQuestion(question);
+let question2 = {
+    question: "question 2",
+    alt: ["ans2", "ans2", "ans2", "ans2"],
+    correctAnswer2: 1
+}
+
+function showQuestion2(q) {
+    var qDiv = document.getElementById("questions");
+    qDiv.textContent = q.question;
+
+    var alts = document.querySelectorAll(".alt");
+
+    alts.forEach(function(element, index) {
+        element.textContent = q.alt[index];
+
+        element.addEventListener("click", function() {
+
+            if (q.correctAnswer2 == index) {
+                console.log("correct");
+                showQuestion3(question3);
+            } else {
+                console.log("wrong answedr")
+                showQuestion3(question3);
+                // timeLeft = (timeLeft.now() - 10000);
+                // minus points and go to next question (2) 
+            }
+        })
+    });
+    return;
+};
+
+
+let question3 = {
+    question: "question 3",
+    alt: ["ans3", "ans3", "ans3", "ans3"],
+    correctAnswer3: 1
+}
+
+function showQuestion3(q) {
+    var qDiv = document.getElementById("questions");
+    qDiv.textContent = q.question;
+
+    var alts = document.querySelectorAll(".alt");
+
+    alts.forEach(function(element, index) {
+        element.textContent = q.alt[index];
+
+        element.addEventListener("click", function() {
+
+            if (q.correctAnswer3 == index) {
+                console.log("correct");
+                showQuestion4(question4);
+            } else {
+                console.log("wrong answedr")
+                showQuestion4(question4);
+                // timeLeft = (timeLeft.now() - 10000);
+                // minus points and go to next question (2) 
+            }
+        })
+    });
+    return;
+};
+
+
+let question4 = {
+    question: "question 4",
+    alt: ["ans4", "ans4", "ans4", "ans4"],
+    correctAnswer4: 1
+}
+
+function showQuestion4(q) {
+    var qDiv = document.getElementById("questions");
+    qDiv.textContent = q.question;
+
+    var alts = document.querySelectorAll(".alt");
+
+    alts.forEach(function(element, index) {
+        element.textContent = q.alt[index];
+
+        element.addEventListener("click", function() {
+
+            if (q.correctAnswer4 == index) {
+                console.log("correct");
+                endGame();
+            } else {
+                console.log("wrong answedr")
+                endGame();
+            }
+        })
+    });
+    return;
+};
+
+
+
+
+function endGame() {
+    location.href = "./highscores.html";
+}
 
 
 
 
 
-// function startQuiz() {
-//     countdown();
-//     console.log("stardsa")
-//         // getNewQuestion();
-
-
-// }
-
-// startQuiz();
-
-// function getNewQuestion() {
-//     if (document.getElementById("correct").clicked == true) {
-//         // element.classList.add("hide");
-//         console.log();
-//     }
-// }
+showQuestion(question1);
 
 
 
 
 
+countdown();
 
 
+var timerEl = document.getElementById("countdown")
 
+function countdown() {
+    var timeLeft = 60;
 
+    var timeInterval = setInterval(function() {
+            if (timeLeft > 1) {
+                timerEl.textContent = timeLeft + " seconds remaining";
+                timeLeft--;
+            } else if (timeLeft === 1) {
+                timerEl.textContent = timeLeft + " second remaining";
+                timeLeft--;
+            } else {
+                timerEl.textContent = " ";
+                endGame();
+                clearInterval(timeInterval);
 
-
-
-
-
-// var getNewQuestion = function() {
-//     if (document.querySelector(".show")) {
-//         document.querySelector(".show").remove();
-//     }
-//     // questionHeaderEl.removeChild(document.createElement("h1"));
-//     if (!questions[currentQuestion]) {
-//         return;
-//     }
-
-//     var questionEl = document.createElement("h1");
-//     questionEl.textContent = questions[currentQuestion].question;
-//     questionHeaderEl.appendChild(questionEl);
-
-
-//     var answersEl = document.createElement("div");
-//     answersEl.className = "show"
-
-//     answerBoxEl.appendChild(answersEl)
-
-//     for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-//         var buttonEl = document.createElement("button");
-//         buttonEl.textContent = questions[currentQuestion].choices[i];
-//         answersEl.appendChild(buttonEl);
-//     }
-// }
-
-// var answerSelect() {
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var questionHeaderEl = document.querySelector(".question");
-// var answerBoxEl = document.querySelector(".answers");
-// var currentQuestion = 0;
-// var userScore = 60;
-
-
-// var timerEl = document.getElementById("countdown")
-
-// function countdown() {
-//     var timeLeft = 60;
-
-//     var timeInterval = setInterval(function() {
-//             if (timeLeft > 1) {
-//                 timerEl.textContent = timeLeft + " seconds remaining";
-//                 timeLeft--;
-//             } else if (timeLeft === 1) {
-//                 timerEl.textContent = timeLeft + " second remaining";
-//                 timeLeft--;
-//             } else {
-//                 timerEl.textContent = " ";
-//                 clearInterval(timeInterval);
-
-//                 endQuiz();
-//             }
-//         },
-//         1000);
-// }
+            }
+        },
+        1000);
+}
